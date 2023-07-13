a5903ec56587e0dc8b0bfba35ce15450ba07c380
kambleps <125243027+kambleps@users.noreply.github.com>
2023-07-13 11:51:39 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+   let[todo,setTodo]=useState("");
+    let[add,setAdd]=useState([]);
+
+    function addonlist(){
+        setAdd([...add,todo])
+    }
+    function delet(index){
+         const updatedtodo = [...add];
+         updatedtodo.splice(index,1)
+         setAdd(updatedtodo);
+    }
+   
+    return(
+        <div>
+         <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}></input>
+         <button onClick={addonlist}>Add Todo</button>
+         {
+            add.map((list,index)=>(
+                <div key={index}>
+                    <p>{list}
+                    <button onClick={()=>delet(index)}>Delete</button>
+                    </p>
+                    
+                </div>
+            ))
+        }
+        </div>
+        
+    )
 }
 
 export default App
